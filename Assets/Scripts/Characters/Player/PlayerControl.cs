f2e83424bc1be3b69ec2c57eb03b868d7a50f622
Migu <2500512459@qq.com>
2025-05-17 23:34:36 +0800

    添加动画

--- Assets/Scripts/Characters/Player/PlayerControl.cs
+++ Assets/Scripts/Characters/Player/PlayerControl.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private float speed = 5;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
             float x = Input.GetAxisRaw("Horizontal");
             Debug.Log(x);
-            transform.position  = new Vector3(transform.position.x + x * speed * Time.deltaTime, transform.position.y, transform.position.z);
+            if (x == 1)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                //·­×ª
+                spriteRenderer.flipX  = true;
+            }
+            transform.position = new Vector3(transform.position.x + x * speed * Time.deltaTime, transform.position.y, transform.position.z);
+            anim.Play("Player_run");
+        }
+        else
+        {
+            anim.Play("Player_idle");
         }
     }
 }
