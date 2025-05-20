97b1d57f9c735577d970ca4c4a05c6d544e460f9
Migu <2500512459@qq.com>
2025-05-20 08:41:13 +0800

    添加注释

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -5,12 +5,12 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
     public PlayerInputAction PlayerInputAction;
-    public bool Jump => PlayerInputAction.GamePlay.Jump.WasPressedThisFrame();
-    public bool StopJump => PlayerInputAction.GamePlay.Jump.WasReleasedThisFrame();
-    public Vector2 Axes => PlayerInputAction.GamePlay.Axes.ReadValue<Vector2>();
-    public float AxisX => Axes.x;
-    public float AxisY => Axes.y;
-    public bool Move => AxisX != 0f;
+    public bool Jump => PlayerInputAction.GamePlay.Jump.WasPressedThisFrame(); // 检测到跳跃
+    public bool StopJump => PlayerInputAction.GamePlay.Jump.WasReleasedThisFrame(); // 检测到取消跳跃
+    public Vector2 Axes => PlayerInputAction.GamePlay.Axes.ReadValue<Vector2>(); //获取输入轴
+    public float AxisX => Axes.x; // 获取x轴
+    public float AxisY => Axes.y; //  获取y轴
+    public bool Move => AxisX != 0f; //检测是否移动
     public void Awake()
     {
         PlayerInputAction = new PlayerInputAction();
